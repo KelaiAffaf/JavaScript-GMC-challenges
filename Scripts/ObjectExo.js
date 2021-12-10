77a08c54392cab73d7099609f05b57630eefa7c0
Afaf KELAI <ea_kelai@esi.dz>
2021-12-10 16:02:49 +0100

    Level one Objects

--- Scripts/ObjectExo.js
+++ Scripts/ObjectExo.js
@@ -15,13 +15,28 @@ dog.age = 3;
 dog.bark = function () {
     return "woof woof";
 }
+console.log(dog);
 console.log(dog.name);
 console.log(dog.legs);
 console.log(dog.color);
 console.log(dog.age);
 console.log(dog.bark());
+// get name, legs, color, age and bark value from the dog object
+console.log(Object.values(dog));// ['Boddy', '4', 'brown', '3', 'woof woof']
+//using for in 
+for (let key in dog) {
+    console.log(key);// name, legs, color, age, bark
+    console.log(dog[key]);//Boddy, 4, brown, 3, woof woof
+    console.log(`${key} : ${dog[key]}`);// name : Boddy, legs : 4, color : brown, age : 3, bark : woof woof
+}
+//***********/
 dog.breed = "Lab";
 dog.getDogInfo = function () {
     return `${this.name} is a ${this.breed} dog.`;
 }
 console.log(dog.getDogInfo());
+
+
+// ### **Exercises: Level 2**
+// 1. Find the person who has many skills in the users object.
+// 2. Count logged in users,count users having greater than equal to 50 points from the following object.
