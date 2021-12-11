ad51412d7ee79b9b04bfff3cfc5a5d4ecad8ec01
Afaf KELAI <ea_kelai@esi.dz>
2021-12-11 16:03:36 +0100

    string web2

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -14,3 +14,51 @@ function nameOfFunction (paremeter){
 // *? 4. Self Invoking Functions 
 // *? 5. **Arrow Function**
 
+
+const printFullName = (firstName, lastName) => {
+    
+    return `${firstName} and  ${lastName}`
+  }
+  
+  console.log(printFullName('Afaf', 'kelai'))
+
+//   Quadratic equation is calculated as follows: ax2 + bx + c = 0. 
+// Write a function which calculates value or values of a quadratic equation, solveQuadEquation.
+
+// Delta is the value of b2 - 4ac.
+//  si delta>0, the equation has two real roots., x1 and x2. x1= (-b + sqrt(delta))/2a, x2= (-b - sqrt(delta))/2a.
+// if delta==0 the equation has one real root, x= -b/2a.
+// if delta<0, the equation has no real roots.
+
+const quadraticequation=(a, b, c)=> {
+    let delta = b^2 - 4 * a * c;
+    if (delta > 0) {
+        let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+        let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+        return `${x1} and ${x2}`;
+    } else if (delta == 0) {
+        let x = -b / (2 * a);
+        return x;
+    } else {
+        return "No real roots";
+    }
+}
+// using switch case
+const quadraticequation2=(a, b, c)=> {
+    let delta = b^2 - 4 * a * c;
+    switch (true) {
+        case (delta > 0):
+            let x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            let x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            return `${x1} and ${x2}`;
+        case (delta == 0):
+            let x = -b / (2 * a);
+            return x;
+        default:
+            return "No real roots";
+    }
+}
+
+console.log(quadraticequation(1, -5, 6));
+console.log(quadraticequation(1, 2, 1));
+console.log(quadraticequation(1, -2, 1));
---
+++ Scripts/string.js
@@ -0,0 +1,17 @@
+// typeOf()==> it returns the type of the variable
+// let a = "hello"; console.log (typeof a);// string
+
+// Object String methodes 
+// string.length ==> it returns the length of the string
+// string.capitalize() ==> it capitalizes the first letter of the string
+let message = "hello world";
+console.log(message.toUpperCase());
+// toUpperCase() ==> it converts the string to uppercase
+let message2="HELLO WORLD";
+console.log(message2.toLowerCase());
+// toLowerCase() ==> it converts the string to lowercase
+// string.charAt(index) ==> it returns the character at the specified index
+// string.indexOf(searchValue,fromIndex) ==> it returns the index of the first occurrence of the specified value in the string
+console.log(message.indexOf("o"));
+// string.split(separator,limit) ==> it splits the string into an array of substrings
+console.log(message.split(" "));
