042fa40ada2329874365931de538b4c026f9695e
Afaf KELAI <ea_kelai@esi.dz>
2021-12-11 12:07:19 +0100

    rate products with for loop

--- Scripts/ObjectExo2.js
+++ Scripts/ObjectExo2.js
@@ -1,265 +1,314 @@
 const users = [
-    {
-        _id: 'ab12ex',
-        username: 'Alex',
-        email: 'alex@alex.com',
-        password: '123123',
-        createdAt:'08/01/2020 9:00 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'fg12cy',
-        username: 'Asab',
-        email: 'asab@asab.com',
-        password: '123456',
-        createdAt:'08/01/2020 9:30 AM',
-        isLoggedIn: true
-    },
-    {
-        _id: 'zwf8md',
-        username: 'Brook',
-        email: 'brook@brook.com',
-        password: '123111',
-        createdAt:'08/01/2020 9:45 AM',
-        isLoggedIn: true
-    },
-    {😩
-        _id: 'eefamr',
-        username: 'Martha',
-        email: 'martha@martha.com',
-        password: '123222',
-        createdAt:'08/01/2020 9:50 AM',
-        isLoggedIn: false
-    },
-    {
-        _id: 'ghderc',
-        username: 'Thomas',
-        email: 'thomas@thomas.com',
-        password: '123333',
-        createdAt:'08/01/2020 10:00 AM',
-        isLoggedIn: false
-    }
-    ];
-
-    const products = [
   {
-    _id: 'eedfcf',
-    name: 'mobile phone',
-    description: 'Huawei Honor',
+    _id: "ab12ex",
+    username: "Alex",
+    email: "alex@alex.com",
+    password: "123123",
+    createdAt: "08/01/2020 9:00 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "fg12cy",
+    username: "Asab",
+    email: "asab@asab.com",
+    password: "123456",
+    createdAt: "08/01/2020 9:30 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "zwf8md",
+    username: "Brook",
+    email: "brook@brook.com",
+    password: "123111",
+    createdAt: "08/01/2020 9:45 AM",
+    isLoggedIn: true,
+  },
+  {
+    _id: "eefamr",
+    username: "Martha",
+    email: "martha@martha.com",
+    password: "123222",
+    createdAt: "08/01/2020 9:50 AM",
+    isLoggedIn: false,
+  },
+  {
+    _id: "ghderc",
+    username: "Thomas",
+    email: "thomas@thomas.com",
+    password: "123333",
+    createdAt: "08/01/2020 10:00 AM",
+    isLoggedIn: false,
+  },
+];
+
+const products = [
+  {
+    _id: "eedfcf",
+    name: "mobile phone",
+    description: "Huawei Honor",
     price: 200,
     ratings: [
-      { userId: 'fg12cy', rate: 5 },
-      { userId: 'zwf8md', rate: 4.5 }
+      { userId: "fg12cy", rate: 5 },
+      { userId: "zwf8md", rate: 4.5 },
     ],
-    likes: []
+    likes: [],
   },
   {
-    _id: 'aegfal',
-    name: 'Laptop',
-    description: 'MacPro: System Darwin',
+    _id: "aegfal",
+    name: "Laptop",
+    description: "MacPro: System Darwin",
     price: 2500,
     ratings: [],
-    likes: ['fg12cy']
+    likes: ["fg12cy"],
   },
   {
-    _id: 'hedfcg',
-    name: 'TV',
-    description: 'Smart TV:Procaster',
+    _id: "hedfcg",
+    name: "TV",
+    description: "Smart TV:Procaster",
     price: 400,
-    ratings: [{ userId: 'fg12cy', rate: 5 }],
-    likes: ['fg12cy']
-  }
-]
-// signUp function 
+    ratings: [{ userId: "fg12cy", rate: 5 }],
+    likes: ["fg12cy"],
+  },
+];
+// signUp function
 function signUp(users, user) {
-    // check if the user already exists
-    const userExists = users.find(u => u.email === user.email);
-    if (userExists) {
-        return {
-            success: false,
-            message: 'User already exists'
-        }
-    }
-    // if user doesn't exist, add new user
-    const newUser = {
-        _id: Math.random().toString(36).substr(2, 9),
-        username: user.username,
-        email: user.email,
-        password: user.password,
-        createdAt: new Date().toLocaleString()
-    }
-    users.push(newUser);
+  // check if the user already exists
+  const userExists = users.find((u) => u.email === user.email);
+  if (userExists) {
     return {
-        success: true,
-        user: newUser
-    }
+      success: false,
+      message: "User already exists",
+    };
+  }
+  // if user doesn't exist, add new user
+  const newUser = {
+    _id: Math.random().toString(36).substr(2, 9),
+    username: user.username,
+    email: user.email,
+    password: user.password,
+    createdAt: new Date().toLocaleString(),
+  };
+  users.push(newUser);
+  return {
+    success: true,
+    user: newUser,
+  };
 }
 // signIn function
 function signIn(users, user) {
-    // check if the user exists
-    const userExists = users.find(u => u.email === user.email);
-    if (!userExists) {
-        return {
-            success: false,
-            message: 'User does not exist'
-        }
-    }
-    // if user exists, check if password is correct
-    if (userExists.password === user.password) {
-        return {
-            success: true,
-            user: userExists
-            message: 'User logged in'
-        }
-    }
+  // check if the user exists
+  const userExists = users.find((u) => u.email === user.email);
+  if (!userExists) {
     return {
-        success: false,
-        message: 'Password is incorrect'
-    }
+      success: false,
+      message: "User does not exist",
+    };
+  }
+  // if user exists, check if password is correct
+  if (userExists.password === user.password) {
+    return {
+      success: true,
+      user: userExists,
+      message: "User logged in",
+    };
+  }
+  return {
+    success: false,
+    message: "Password is incorrect",
+  };
 }
-// rateProduct 
+// rateProduct
 function rateProduct(products, productId, userId, rate) {
-    // check if the product exists
-    const productExists = products.find(p => p._id === productId);
-    if (!productExists) {
-        return {
-            success: false,
-            message: 'Product does not exist'
-        }
-    }
-    // check if the user exists
-    const userExists = users.find(u => u._id === userId);
-    if (!userExists) {
-        return {
-            success: false,
-            message: 'User does not exist'
-        }
-    }
-    // if user and product exists, add the rating
+  // check if the product exists
+  const productExists = products.find((p) => p._id === productId);
+  if (!productExists) {
+    return {
+      success: false,
+      message: "Product does not exist",
+    };
+  }
+  // check if the user exists
+  const userExists = users.find((u) => u._id === userId);
+  if (!userExists) {
+    return {
+      success: false,
+      message: "User does not exist",
+    };
+  }
+  // if user and product exists, add the rating
+  // check if this user already rated the product
+  const userRated = productExists.ratings.find((r) => r.userId === userId);
+  if (userRated) {
+    userRated.rate = rate;
+    return {
+      success: true,
+      message: "Product rate updated",
+    };
+  } else {
     productExists.ratings.push({ userId: userId, rate: rate });
     return {
-        success: true,
-        message: 'Rating added'
-    }
+      success: true,
+      message: "Rating added",
+    };
+  }
 }
+console.log(rateProduct(products, "aegfal", "fg12cy", 5));
+console.log(products);
+
+// rate product using for loop
+function rateProduct2(products, productId, userId, rate) {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i]._id === productId) {
+      const product = products[i];
+      // check if the user exists
+      for (let j = 0; j < users.length; j++) {
+        if (users[j]._id === userId) {
+          const user = users[j];
+          // check if this user already rated the product we update it
+          for (let k = 0; k < product.ratings.length; k++) {
+            if (product.ratings[k].userId === userId) {
+              product.ratings[k].rate = rate;
+              return {
+                success: true,
+                product: product,
+                user: user,
+                success: true,
+                message: "Product rate updated",
+              };
+            }
+          }
+        }
+      }
+      // if user doesn't exist, add the rating
+      product.ratings.push({ userId: userId, rate: rate });
+      return {
+        product: product,
+        success: true,
+        message: "Rating added",
+      };
+    }
+  }
+}
+console.log(rateProduct2(products, "aegfal", "fg12cy", 5));
+console.log(rateProduct2(products, "aegfal", "fg12cy", 3));
+
 // likeProduct
 function likeProduct(products, productId, userId) {
-    // check if the product exists
-    const productExists = products.find(p => p._id === productId);
-    if (!productExists) {
-        return {
-            success: false,
-            message: 'Product does not exist'
-        }
-    }
-    // check if the user exists
-    const userExists = users.find(u => u._id === userId);
-    if (!userExists) {
-        return {
-            success: false,
-            message: 'User does not exist'
-        }
-    }
-    // if user and product exists, add the like
-    productExists.likes.push(userId);
+  // check if the product exists
+  const productExists = products.find((p) => p._id === productId);
+  if (!productExists) {
     return {
-        success: true,
-        message: 'Product liked'
-    }
+      success: false,
+      message: "Product does not exist",
+    };
+  }
+  // check if the user exists
+  const userExists = users.find((u) => u._id === userId);
+  if (!userExists) {
+    return {
+      success: false,
+      message: "User does not exist",
+    };
+  }
+  // if user and product exists, add the like
+  productExists.likes.push(userId);
+  return {
+    success: true,
+    message: "Product liked",
+  };
 }
 // getProducts
 function getProducts(products) {
-    return {
-        success: true,
-        products: products
-    }
+  return {
+    success: true,
+    products: products,
+  };
 }
 // getProduct
 function getProduct(products, productId) {
-    // check if the product exists
-    const productExists = products.find(p => p._id === productId);
-    if (!productExists) {
-        return {
-            success: false,
-            message: 'Product does not exist'
-        }
-    }
+  // check if the product exists
+  const productExists = products.find((p) => p._id === productId);
+  if (!productExists) {
     return {
-        success: true,
-        product: productExists
-    }
+      success: false,
+      message: "Product does not exist",
+    };
+  }
+  return {
+    success: true,
+    product: productExists,
+  };
 }
 // getUsers
 function getUsers(users) {
-    return {
-        success: true,
-        users: users
-    }
+  return {
+    success: true,
+    users: users,
+  };
 }
 // getUser
 function getUser(users, userId) {
-    // check if the user exists
-    const userExists = users.find(u => u._id === userId);
-    if (!userExists) {
-        return {
-            success: false,
-            message: 'User does not exist'
-        }
-    }
+  // check if the user exists
+  const userExists = users.find((u) => u._id === userId);
+  if (!userExists) {
     return {
-        success: true,
-        user: userExists
-    }
+      success: false,
+      message: "User does not exist",
+    };
+  }
+  return {
+    success: true,
+    user: userExists,
+  };
 }
 // getUserRatings
 function getUserRatings(users, userId) {
-    // check if the user exists
-    const userExists = users.find(u => u._id === userId);
-    if (!userExists) {
-        return {
-            success: false,
-            message: 'User does not exist'
-        }
-    }
+  // check if the user exists
+  const userExists = users.find((u) => u._id === userId);
+  if (!userExists) {
     return {
-        success: true,
-        ratings: userExists.ratings
-    }
+      success: false,
+      message: "User does not exist",
+    };
+  }
+  return {
+    success: true,
+    ratings: userExists.ratings,
+  };
 }
 // getUserLikes
 function getUserLikes(users, userId) {
-    // check if the user exists
-    const userExists = users.find(u => u._id === userId);
-    if (!userExists) {
-        return {
-            success: false,
-            message: 'User does not exist'
-        }
-    }
+  // check if the user exists
+  const userExists = users.find((u) => u._id === userId);
+  if (!userExists) {
     return {
-        success: true,
-        likes: userExists.likes
-    }
+      success: false,
+      message: "User does not exist",
+    };
+  }
+  return {
+    success: true,
+    likes: userExists.likes,
+  };
 }
 // avergeRating
 function averageRating(products, productId) {
-    // check if the product exists
-    const productExists = products.find(p => p._id === productId);
-    if (!productExists) {
-        return {
-            success: false,
-            message: 'Product does not exist'
-        }
-    }
-    let sum = 0;
-    let count = 0;
-    productExists.ratings.forEach(r => {
-        sum += r.rate;
-        count++;
-    });
+  // check if the product exists
+  const productExists = products.find((p) => p._id === productId);
+  if (!productExists) {
     return {
-        success: true,
-        average: sum / count
-    }
+      success: false,
+      message: "Product does not exist",
+    };
+  }
+  let sum = 0;
+  let count = 0;
+  productExists.ratings.forEach((r) => {
+    sum += r.rate;
+    count++;
+  });
+  return {
+    success: true,
+    average: sum / count,
+  };
 }
-
